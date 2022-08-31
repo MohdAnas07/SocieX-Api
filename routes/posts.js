55ff599026f17159c35841c0d6ba614c0b889cea
MohdAnas07 <anas98144@gmail.com>
2022-08-31 17:14:38 +0530

    complete backend api for SocieX for now

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 // CREATE POST
 
@@ -66,7 +67,35 @@ router.put('/:id/like', async (req, res) => {
     }
 })
 
+
 // TIMELINE POSTS
+router.get('/timeline/all', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.body.userId)
+        const userPosts = await Post.find({ userId: currentUser._id });
+        const friendPosts = await Promise.all(
+            currentUser.followings.map(friendId => {
+                return Post.find({ userId: friendId })
+            })
+        )
+
+        res.status(200).json(userPosts.concat(...friendPosts));
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+
+// GET A POST
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 
 
