ff514f665b5b5b1753e009d21e6476e659437769
MohdAnas07 <anas98144@gmail.com>
2022-08-31 16:38:39 +0530

    like post and dislike post api functionality done

--- routes/posts.js
+++ routes/posts.js
@@ -48,8 +48,24 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
-// GET A POST
 // LIKE A POST
+
+router.put('/:id/like', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({ $push: { likes: req.body.userId } })
+            res.status(200).json('you like this post')
+        } else {
+            await post.updateOne({ $pull: { likes: req.body.userId } })
+            res.status(200).json('you dislike this post')
+        }
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 // TIMELINE POSTS
 
 
